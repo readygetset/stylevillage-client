bdcad63da7afe9e89a36836e3e6597a10e2252dc
Jaemin Kim <jay.kimjaemin@gmail.com>
2023-12-30 11:09:20 +0900

    feat: Bottom Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Box } from '@mui/material';
+import BottomNav from './components/BottomNavigation';
 import './App.css';
 
 function App() {
-  return <div>div</div>;
+  return (
+    <div>
+      <Box height="85vh">ddd</Box>
+      <BottomNav />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/BottomNavigation/index.tsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export default function BottomNav() {
+  return (
+    <Box sx={{ width: '100%', height: '15vh' }}>
+      <BottomNavigation showLabels>
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
