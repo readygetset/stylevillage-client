eb39985f9029e7877934fe3727e9b4983c7b830e
Jaemin Kim <jay.kimjaemin@gmail.com>
2023-12-30 19:56:41 +0900

    feat: register handler

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,35 @@
 import { Box, Button, Divider, Stack, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export function RegisterPage() {
+  const [input, setInput] = useState({
+    id: '',
+    password: '',
+    passwordCheck: '',
+    lastName: '',
+    firstName: '',
+    age: 0,
+  });
+  const [isPasswordSame, setIsPasswordSame] = useState(true);
+
+  function checkPassword(passwordCheck: string) {
+    if (input.password === passwordCheck) {
+      return true;
+    }
+    return false;
+  }
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.id === 'passwordCheck') {
+      setIsPasswordSame(checkPassword(event.target.value));
+    }
+
+    setInput({
+      ...input,
+      [event.target.id]: event.target.value,
+    });
+  }
+
   return (
     <Box padding={2} paddingTop={4}>
       <Box marginBottom={4} textAlign={'center'}>
@@ -8,17 +37,22 @@ export function RegisterPage() {
       </Box>
       <Box>
         <Stack spacing={3}>
-          <TextField required id="id" label="아이디" />
-          <TextField required id="password" label="비밀번호" />
-          <TextField required id="password-check" label="비밀번호 확인" />
+          <TextField required id="id" label="아이디" onChange={handleInput} />
+          <TextField required id="password" type="password" label="비밀번호" onChange={handleInput} />
+          <TextField required id="passwordCheck" type="password" label="비밀번호 확인" onChange={handleInput} />
+          {!isPasswordSame && (
+            <Typography variant="body2" color="error">
+              비밀번호가 일치하지 않습니다.
+            </Typography>
+          )}
         </Stack>
         <Box marginY={2}>
           <Divider />
         </Box>
         <Stack spacing={2}>
-          <TextField required id="last-name" label="성" />
-          <TextField required id="first-name" label="이름" />
-          <TextField required id="age" label="나이" type="number" />
+          <TextField required id="lastName" label="성" onChange={handleInput} />
+          <TextField required id="firstName" label="이름" onChange={handleInput} />
+          <TextField required id="age" label="나이" type="number" onChange={handleInput} />
         </Stack>
       </Box>
       <Box paddingY={6}>
