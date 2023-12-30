746de161a029bcc26710e8f9d5794e428003a8d8
Jaemin Kim <jay.kimjaemin@gmail.com>
2023-12-30 18:58:09 +0900

    fix: icon, text change at BottomNav

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,17 +1,23 @@
+/**
+ * 아이콘 목록은 아래 링크에서 확인 가능.
+ * 버튼 누르면 바로 복사해서 사용할 수 있음.
+ * https://mui.com/material-ui/material-icons/
+ */
+
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 export default function BottomNav() {
   return (
     <Box sx={{ width: '100%', height: '10%' }}>
       <BottomNavigation showLabels sx={{ height: '100%', backgroundColor: 'aliceblue' }}>
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="목록" icon={<FormatListBulletedIcon />} />
+        <BottomNavigationAction label="대화" icon={<ChatBubbleOutlineIcon />} />
+        <BottomNavigationAction label="내 정보" icon={<AccountCircleIcon />} />
       </BottomNavigation>
     </Box>
   );
