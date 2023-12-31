a4e265b1b1f169e83c3e0822deecba9b3105c7d2
Jaemin Kim <jay.kimjaemin@gmail.com>
2023-12-31 19:36:24 +0900

    feat: pretendard font

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 import { RouteComponent } from './route';
 import { Layout } from './components/Layout';
 
 import './App.css';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Pretendard-Regular',
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <RouteComponent />
-      </Layout>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Layout>
+          <RouteComponent />
+        </Layout>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
