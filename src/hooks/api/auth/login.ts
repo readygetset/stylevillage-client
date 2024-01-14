a5fd5f5cffa22e1d807e30eaf27b061ee3bb11b2
readygetset <ksp7292@gmail.com>
2024-01-14 15:05:08 +0900

    refactor: api seperation

---
+++ src/hooks/api/auth/login.ts
@@ -0,0 +1,29 @@
+import { enqueueSnackbar } from 'notistack';
+import axios, { AxiosError } from 'axios';
+
+import { LOGIN_MESSAGE } from '../../../data/messages';
+
+export interface Login {
+  username: string;
+  password: string;
+}
+
+export async function postLoginAPICall(values: Login) {
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, values);
+    if (response.status === 200) {
+      const { nickname, isBannded, accessToken } = response.data;
+      sessionStorage.setItem('accessToken', accessToken);
+      enqueueSnackbar(`${nickname}님 안녕하세요 :)`, { variant: 'success' });
+      if (isBannded === true) {
+        enqueueSnackbar(`현재 ${nickname}님의 계정은 사용 정지되었습니다.`, { variant: 'warning' });
+      }
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      enqueueSnackbar(err.response?.data?.message ?? LOGIN_MESSAGE.LOGIN_FAIL, { variant: 'error' });
+    } else {
+      enqueueSnackbar(LOGIN_MESSAGE.LOGIN_FAIL, { variant: 'error' });
+    }
+  }
+}
