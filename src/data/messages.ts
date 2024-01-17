88c1a0d045ff8f749209256c5e78414da78c49fb
readygetset <ksp7292@gmail.com>
2024-01-17 19:39:32 +0900

    feat: arrived apply

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -5,3 +5,9 @@ export const LOGIN_MESSAGE = {
 export const SEARCH_MESSAGE = {
   SEARCH_FAIL: '검색에 실패하였습니다 :(',
 };
+
+export const APPLY_MESSAGE = {
+  APPLY_FAIL: '대여 신청을 불러오지 못했습니다 :(',
+  LEND_CREATE_FAIL: '대여 신청 승인에 문제가 생겼어요 :(',
+  APPLY_REJECT_FAIL: '대여 신청 거절에 문제가 생겼어요 :(',
+};
