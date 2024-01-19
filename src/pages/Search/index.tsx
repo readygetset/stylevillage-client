2af257a4d482139aae8631361883db4b49091f5c
readygetset <ksp7292@gmail.com>
2024-01-19 15:45:06 +0900

    fix: search design fix

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import { useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 
 import { getSearchAPICall } from '../../hooks/api/search/search';
 import SearchBar from '../../components/SearchBar';
@@ -36,12 +36,6 @@ export function SearchPage() {
   const seasonSelected = searchParams.getAll('season');
   const filterSelected = searchParams.getAll('status');
 
-  const defaultResult: JSX.Element = (
-    <Typography width="100%" textAlign="center" sx={{ mt: '50px', color: 'gray', fontSize: 20 }}>
-      검색 결과가 없어요
-    </Typography>
-  );
-
   const emptySearchArray: SearchClothesRes[] = [];
   const [searchedClothes, setSearchedClothes] = useState(emptySearchArray);
   const handleSearch = async (query: string) => {
@@ -56,26 +50,6 @@ export function SearchPage() {
   };
   // handleSearch(url);
 
-  const clothesCards = searchedClothes
-    ? searchedClothes.map((clothes: SearchClothesRes) => {
-        const { id, name, status, owner, isWished } = clothes;
-        return (
-          <Box sx={{ margin: 3, display: 'inline-block' }}>
-            <ClothesPreviewCard
-              key={id}
-              clothesId={id || 0}
-              clothesname={name}
-              imgsrc={''}
-              status={status}
-              userid={owner.id || 0}
-              username={'닉네임'}
-              isWished={isWished}
-            />
-          </Box>
-        );
-      })
-    : [];
-
   return (
     <>
       <Box sx={{ paddingTop: '24px', backgroundColor: '#E9E9E9' }}>
@@ -87,7 +61,34 @@ export function SearchPage() {
           handleSearch={handleSearch}
         />
       </Box>
-      {clothesCards && clothesCards.length > 0 ? clothesCards : defaultResult}
+      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} sx={{ mt: 5 }}>
+        {searchedClothes?.length ? (
+          <Box display={'flex'} width={1200}>
+            <Grid container spacing={3} justifyContent={'flex-start'} display={'flex'} flexDirection={'row'}>
+              {searchedClothes.map((clothes) => (
+                <Grid item key={clothes.id}>
+                  <ClothesPreviewCard
+                    key={clothes.id}
+                    clothesId={clothes.id || 0}
+                    clothesname={clothes.name}
+                    imgsrc={''}
+                    status={clothes.status}
+                    userid={clothes.owner.id || 0}
+                    username={'닉네임'}
+                    isWished={clothes.isWished}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        ) : (
+          <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+            <Typography variant="h6" sx={{ color: 'gray', mt: 5 }}>
+              검색 결과에 맞는 옷이 없어요
+            </Typography>
+          </Box>
+        )}
+      </Box>
     </>
   );
 }
